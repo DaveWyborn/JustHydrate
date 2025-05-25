8a8896d6bc53e636e42891a38553271b6f51f9aa
DaveWyborn <dwyborn@gmail.com>
2025-05-25 21:04:08 +0100

    Initial Commit

---
+++ JustHydrate/JustHydrateApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustHydrateApp.swift
+//  JustHydrate
+//
+//  Created by Dave Wyborn on 25/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct JustHydrateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JustHydrateTests/JustHydrateTests.swift
@@ -0,0 +1,17 @@
+//
+//  JustHydrateTests.swift
+//  JustHydrateTests
+//
+//  Created by Dave Wyborn on 25/05/2025.
+//
+
+import Testing
+@testable import JustHydrate
+
+struct JustHydrateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
